36eee6a448b2e39cbf15a123cabfcf70b608d57a
kitajii <kazu.rmcf@gmail.com>
2021-04-24 05:20:31 +0000

    マップ画面に現在地をマーカーで表示

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -4,7 +4,63 @@
 @section('title','マップ')
 
 @section('head-script')
-    <script src="https://maps.google.com/maps/api/js?key=AIzaSyAQ7GF8UWRS03uApoVVzF2v_3a4VrFPxpo&language=ja"></script>
+    <script>
+    // 現在地取得処理
+    
+    function initMap() {
+        // Geolocation APIに対応している
+        if (navigator.geolocation) {
+            // 現在地を取得(watchPositionで自動更新)
+            navigator.geolocation.watchPosition(
+                // 取得成功した場合
+                function(position) {
+                    // 緯度・経度を変数に格納
+                    var mapLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                    // 初回のみマップ作成
+                    if(!map) {
+                        // マップオプションを変数に格納
+                        var mapOptions = {
+                            zoom : 15,          // 拡大倍率
+                            center : mapLatLng  // 緯度・経度
+                        };
+                        // マップオブジェクト作成
+                        var map = new google.maps.Map(
+                            document.getElementById('map'), // マップを表示する要素
+                            mapOptions         // マップオプション
+                        );
+                    }
+                    //　マップにマーカーを表示する
+                    var marker = new google.maps.Marker({
+                        map : map,             // 対象の地図オブジェクト
+                        position : mapLatLng   // 緯度・経度
+                    });
+                },
+                // 取得失敗した場合
+                function(error) {
+                // エラーメッセージを表示
+                    switch(error.code) {
+                        case 1: // PERMISSION_DENIED
+                            alert("位置情報の利用が許可されていません");
+                            break;
+                        case 2: // POSITION_UNAVAILABLE
+                            alert("現在位置が取得できませんでした");
+                            break;
+                        case 3: // TIMEOUT
+                            alert("タイムアウトになりました");
+                            break;
+                        default:
+                            alert("その他のエラー(エラーコード:"+error.code+")");
+                            break;
+                    }
+                }
+            );
+        // Geolocation APIに対応していない
+        } else {
+            alert("この端末では位置情報が取得できません");
+        }
+    }
+  </script>
+
 @endsection('head-script')
 
 @section('main')
@@ -18,13 +74,5 @@
 @endsection('main')
 
 @section('foot-script')
-    <script>
-        var MyLatLng = new google.maps.LatLng(35.6811673, 139.7670516);
-        var Options = {
-            zoom: 15,      //地図の縮尺値
-            center: MyLatLng,    //地図の中心座標
-            mapTypeId: 'roadmap'   //地図の種類
-        };
-        var map = new google.maps.Map(document.getElementById('map'), Options);
-    </script>
+   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAQ7GF8UWRS03uApoVVzF2v_3a4VrFPxpo&callback=initMap"></script>
 @endsection('foot-script')
